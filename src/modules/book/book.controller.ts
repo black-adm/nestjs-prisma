c6d3b62bbdabc803d5c8370fca103ed71d6ef392
black-adm <mattheuscontato17@gmail.com>
2023-12-26 05:50:18 -0300

    all CRUD operations finished

--- src/modules/book/book.controller.ts
+++ src/modules/book/book.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { BookService } from './book.service';
+import { BookDto } from './book.dto';
 
 @Controller('book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
+
+  @Post()
+  async create(@Body() data: BookDto) {
+    return this.bookService.create(data);
+  }
+
+  @Get()
+  async findAll() {
+    return this.bookService.findAll();
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() data: BookDto) {
+    return this.bookService.update(id, data);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.bookService.delete(id);
+  }
 }
--- src/modules/book/book.service.ts
+++ src/modules/book/book.service.ts
@@ -21,4 +21,33 @@ export class BookService {
 
     return book;
   }
+
+  async findAll() {
+    return this.prisma.book.findMany();
+  }
+
+  async update(id: string, data: BookDto) {
+    const bookExists = await this.prisma.book.findUnique({
+      where: { id },
+    });
+
+    if (!bookExists) throw new Error('Book does not exists!');
+
+    return await this.prisma.book.update({
+      data,
+      where: { id },
+    });
+  }
+
+  async delete(id: string) {
+    const bookExists = await this.prisma.book.findUnique({
+      where: { id },
+    });
+
+    if (!bookExists) throw new Error('Book does not exists!');
+
+    return await this.prisma.book.delete({
+      where: { id },
+    });
+  }
 }
